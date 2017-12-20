3382235979c270ce4d9c4af68757db21f8405159
haozong <haozongqi@126.com>
2017-12-20 17:34:15 +0800

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,13 @@
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello GitHub</h1></body></html>\n")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
