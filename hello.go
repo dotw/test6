a09bdd0065b15410757a517f317c92efee9a707b
haozong <haozongqi@126.com>
2017-12-20 17:39:30 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
+package main
+
 import (
     "fmt"
     "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello GitHub</h1></body></html>\n")
+    fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello again from GitHub</h1></body></html>\n")
 }
 
 func main() {
